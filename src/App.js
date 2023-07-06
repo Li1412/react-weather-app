370ff7c0b558deb63e2a0e5625b76bd999a231ed
Li1412 <124526316+Li1412@users.noreply.github.com>
2023-07-06 15:24:15 +0200

    added github repository ur

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+<footer>
+  This project is created by Lindelwa Tshuku and is{" "}
+    <a href="https://github.com/Li1412/react-weather-app" target="_blank"> open-sourced on Github</a>
+    </footer>
+    </div>
   );
 }  
         
